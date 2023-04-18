467194054a4b8aead5f0ee78b7365370a51e76ae
tanyasawarn <121800861+tanyasawarn@users.noreply.github.com>
2023-04-18 15:45:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+
+import Header from "./component/Layout/Header";
+ import Meals from "./component/Meals/Meals";
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      <Meals />
+     
+    </Fragment>
+  );
+}
+
+export default App;
