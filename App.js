b9a863141fc81852ac38e4a3a62e8b2e99fc4cb7
tanyasawarn <121800861+tanyasawarn@users.noreply.github.com>
2023-04-19 12:02:37 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import { Fragment } from "react";
 
 import Header from "./component/Layout/Header";
  import Meals from "./component/Meals/Meals";
+import Cart from "./component/Cart/Cart";
 
 function App() {
   return (
     <Fragment>
+    <Cart />
       <Header />
       <Meals />
      
